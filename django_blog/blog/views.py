94e5783d3094036d07a067fcc19aa63ba69666b6
voldown <voldown@outlook.com>
2021-02-05 15:32:30 +0800

    added function views first then upgraded to class-based views.

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,113 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView, ListView
 
-# Create your views here.
+from blog.models import Post, Tag, Category
+from config.models import SideBar
+
+'''
+def post_list(request, category_id=None, tag_id=None):
+    tag = None
+    category = None
+
+    if tag_id:
+        post_list, tag = Post.get_by_tag(tag_id)
+    elif category_id:
+        post_list, category = Post.get_by_category(category_id)
+    else:
+        post_list = Post.latest_posts()
+
+    context = {
+        'category': category,
+        'tag': tag,
+        'post_list': post_list,
+        'sidebars': SideBar.get_all()
+    }
+    context.update(Category.get_navs())
+    return render(request, 'blog/list.html', context=context)
+'''
+
+'''
+def post_detail(request, post_id):
+    try:
+        post = Post.objects.get(id=post_id)
+    except Post.DoesNotExist:
+        post = None
+    context = {
+        'post': post,
+        'sidebars': SideBar.get_all()
+    }
+    context.update(Category.get_navs())
+    return render(request, 'blog/detail.html', context=context)
+'''
+
+
+class PostDetailView(DetailView):
+    queryset = Post.latest_posts()
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
+    pk_url_kwarg = 'post_id'
+
+
+'''
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/list.html'
+    context_object_name = 'post_list'
+    paginate_by = 1
+'''
+
+
+# 用于获取通用数据（侧边栏、分类导航）的混合类视图
+class CommonViewMixin:
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'sidebars': SideBar.get_all(),
+        })
+        context.update(Category.get_navs())
+        return context
+
+
+# 分拆 PostListView 为 IndexView, CategoryView, TagView
+class IndexView(CommonViewMixin, ListView):
+    queryset = Post.latest_posts()
+    paginate_by = 5
+    context_object_name = 'post_list'
+    template_name = 'blog/list.html'
+
+
+class CategoryView(IndexView):
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        category_id = self.kwargs.get('category_id')
+        return queryset.filter(category_id=category_id)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        category_id = self.kwargs.get('category_id')
+        category = get_object_or_404(Category, pk=category_id)
+        context.update({
+            'category': category,
+        })
+        return context
+
+
+class TagView(IndexView):
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        tag_id = self.kwargs.get('tag_id')
+        return queryset.filter(tag__id=tag_id)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        tag_id = self.kwargs.get('tag_id')
+        tag = get_object_or_404(Tag, pk=tag_id)
+        context.update({
+            'tag': tag,
+        })
+        return context
+
+
+
+
+
